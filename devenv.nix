b3d60ecbcb22ea08a1239fcff0aad6dcac64b0f3
Yanda Huang <realyanda@hey.com>
2026-01-24 20:55:06 -0800

    The devenv stuff

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.claude-code
+  ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+  };
+  languages.javascript.bun.enable = true;
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
